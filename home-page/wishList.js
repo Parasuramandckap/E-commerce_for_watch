5079b1a28ea968aded268b954429f90a7b98975a
jeevashhkjs <jeevasdckap@gmail.com>
2023-06-02 16:58:58 +0530

    wishList aligtment not properlyl

--- home-page/wishList.js
+++ home-page/wishList.js
@@ -4,13 +4,14 @@ fetch(`http://localhost:3000/wishlist?_expand=product`)
 .then(res => res.json())
 .then(data =>{
     data.forEach(element => {
-        console.log(element.product)
+        console.log(element)
     });
     let htmlElements = data.map(element => {
         return `
         <div class="productDiv">
             <div class="productImg">
                 <img class="productImg" src="${element.product.images}"/>
+                <img src="./imges-homepage/removeWishList.svg" class="removeWishList" id="${element.product.id}">
             </div>  
             <div class="productLine"></div>
             <div class="productDetails">
@@ -22,4 +23,18 @@ fetch(`http://localhost:3000/wishlist?_expand=product`)
         `
     }).join("")
     wishListDiv.innerHTML = htmlElements
+
+    let removeBtn = document.querySelector(".removeWishList");
+
+    removeBtn.addEventListener("click",(e)=>{
+        let targetId = e.target.id;
+
+        fetch(`http://localhost:3000/wishlist/1`,{
+            method: "DELETE",
+            headers:{'Content-type':'application/json'}
+        })
+
+    })
 })
+
+
